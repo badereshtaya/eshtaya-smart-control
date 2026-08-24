1e9c6cb1b5fa121ddb04ac2b9d2205523afe66b2
badereshtaya <badereshtaya95@gmail.com>
2026-08-24 16:10:16 +0300

    Add integrated permanent light entities

---
+++ custom_components/eshtaya_smart_control/template_manager/light.py
@@ -0,0 +1,58 @@
+"""Light entities managed by Eshtaya Smart Control Template Manager."""
+from __future__ import annotations
+
+from homeassistant.components.light import LightEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .const import DATA_TEMPLATE_MANAGER, SIGNAL_TEMPLATE_CHANGED
+from .entity import TemplateManagedEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    manager = hass.data["eshtaya_smart_control"][DATA_TEMPLATE_MANAGER]
+    known: set[str] = set()
+
+    @callback
+    def add_entities() -> None:
+        entities = []
+        for record in manager.store.templates():
+            if record.get("type") != "light":
+                continue
+            entity_id = str(record["entity_id"])
+            if entity_id in known:
+                continue
+            known.add(entity_id)
+            entities.append(ManagedTemplateLight(hass, manager, record))
+        if entities:
+            async_add_entities(entities)
+
+    add_entities()
+    entry.async_on_unload(async_dispatcher_connect(hass, SIGNAL_TEMPLATE_CHANGED, add_entities))
+
+
+class ManagedTemplateLight(TemplateManagedEntity, LightEntity):
+    """Permanent light wrapper whose source is normally a Tuya switch."""
+
+    @property
+    def is_on(self) -> bool | None:
+        state = self.hass.states.get(self.source_entity)
+        if state is None:
+            return None
+        return state.state == "on"
+
+    async def async_turn_on(self, **kwargs) -> None:
+        await self.hass.services.async_call(
+            "switch", "turn_on", {"entity_id": self.source_entity}, blocking=True
+        )
+
+    async def async_turn_off(self, **kwargs) -> None:
+        await self.hass.services.async_call(
+            "switch", "turn_off", {"entity_id": self.source_entity}, blocking=True
+        )
