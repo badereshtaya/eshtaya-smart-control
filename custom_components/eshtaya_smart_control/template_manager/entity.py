dd76e2bb6ae3fb2e3ddc267f765e3328bfd55136
badereshtaya <badereshtaya95@gmail.com>
2026-08-24 16:15:11 +0300

    Track source state and relinks in template entities

--- custom_components/eshtaya_smart_control/template_manager/entity.py
+++ custom_components/eshtaya_smart_control/template_manager/entity.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.event import async_track_state_change_event
 
 from .const import SIGNAL_TEMPLATE_CHANGED
 
@@ -21,6 +22,8 @@ class TemplateManagedEntity:
         self.entity_id = str(record["entity_id"])
         self._attr_unique_id = str(record.get("unique_id") or f"esc_template::{self.entity_id}")
         self._attr_name = str(record.get("name") or self.entity_id)
+        self._source_unsub = None
+        self._tracked_source = ""
 
     @property
     def source_entity(self) -> str:
@@ -34,6 +37,26 @@ class TemplateManagedEntity:
     async def async_added_to_hass(self) -> None:
         await super().async_added_to_hass()
         self.async_on_remove(async_dispatcher_connect(self.hass, SIGNAL_TEMPLATE_CHANGED, self._template_changed))
+        self._rewire_source_listener()
+
+    @callback
+    def _rewire_source_listener(self) -> None:
+        source = self.source_entity
+        if source == self._tracked_source:
+            return
+        if self._source_unsub:
+            self._source_unsub()
+            self._source_unsub = None
+        self._tracked_source = source
+        if source:
+            self._source_unsub = async_track_state_change_event(
+                self.hass, [source], self._source_changed
+            )
+            self.async_on_remove(self._source_unsub)
+
+    @callback
+    def _source_changed(self, _event) -> None:
+        self.async_write_ha_state()
 
     @callback
     def _template_changed(self) -> None:
@@ -43,4 +66,5 @@ class TemplateManagedEntity:
             return
         self.record = current
         self._attr_name = str(current.get("name") or self.entity_id)
+        self._rewire_source_listener()
         self.async_write_ha_state()
