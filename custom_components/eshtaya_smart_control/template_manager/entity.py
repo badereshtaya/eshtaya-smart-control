c269caa226768036c4c3f5f2ebb49d2a8cf7cb91
badereshtaya <badereshtaya95@gmail.com>
2026-08-24 16:18:06 +0300

    Use manager-owned source availability in template entities

--- custom_components/eshtaya_smart_control/template_manager/entity.py
+++ custom_components/eshtaya_smart_control/template_manager/entity.py
@@ -16,7 +16,6 @@ class TemplateManagedEntity:
     _attr_should_poll = False
 
     def __init__(self, hass: HomeAssistant, manager, record: dict[str, Any]) -> None:
-        self.hass = hass
         self.manager = manager
         self.record = record
         self.entity_id = str(record["entity_id"])
@@ -32,7 +31,7 @@ class TemplateManagedEntity:
 
     @property
     def available(self) -> bool:
-        return self.hass.states.get(self.source_entity) is not None
+        return self.manager.source_available(self.source_entity)
 
     async def async_added_to_hass(self) -> None:
         await super().async_added_to_hass()
@@ -49,10 +48,7 @@ class TemplateManagedEntity:
             self._source_unsub = None
         self._tracked_source = source
         if source:
-            self._source_unsub = async_track_state_change_event(
-                self.hass, [source], self._source_changed
-            )
-            self.async_on_remove(self._source_unsub)
+            self._source_unsub = async_track_state_change_event(self.hass, [source], self._source_changed)
 
     @callback
     def _source_changed(self, _event) -> None:
