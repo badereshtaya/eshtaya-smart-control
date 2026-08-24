c1268561341c35d2266e09ef696463ad9f345979
badereshtaya <badereshtaya95@gmail.com>
2026-08-24 16:27:32 +0300

    Harden Template Manager entity lifecycle cleanup

--- custom_components/eshtaya_smart_control/template_manager/entity.py
+++ custom_components/eshtaya_smart_control/template_manager/entity.py
@@ -16,6 +16,7 @@ class TemplateManagedEntity:
     _attr_should_poll = False
 
     def __init__(self, hass: HomeAssistant, manager, record: dict[str, Any]) -> None:
+        super().__init__()
         self.manager = manager
         self.record = record
         self.entity_id = str(record["entity_id"])
@@ -35,8 +36,17 @@ class TemplateManagedEntity:
 
     async def async_added_to_hass(self) -> None:
         await super().async_added_to_hass()
-        self.async_on_remove(async_dispatcher_connect(self.hass, SIGNAL_TEMPLATE_CHANGED, self._template_changed))
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, SIGNAL_TEMPLATE_CHANGED, self._template_changed)
+        )
         self._rewire_source_listener()
+
+    async def async_will_remove_from_hass(self) -> None:
+        if self._source_unsub:
+            self._source_unsub()
+            self._source_unsub = None
+        self._tracked_source = ""
+        await super().async_will_remove_from_hass()
 
     @callback
     def _rewire_source_listener(self) -> None:
@@ -48,7 +58,9 @@ class TemplateManagedEntity:
             self._source_unsub = None
         self._tracked_source = source
         if source:
-            self._source_unsub = async_track_state_change_event(self.hass, [source], self._source_changed)
+            self._source_unsub = async_track_state_change_event(
+                self.hass, [source], self._source_changed
+            )
 
     @callback
     def _source_changed(self, _event) -> None:
@@ -58,7 +70,9 @@ class TemplateManagedEntity:
     def _template_changed(self) -> None:
         current = self.manager.store.get(self.entity_id)
         if current is None:
-            self.hass.async_create_task(self.async_remove(), f"Remove template entity {self.entity_id}")
+            self.hass.async_create_task(
+                self.async_remove(), f"Remove template entity {self.entity_id}"
+            )
             return
         self.record = current
         self._attr_name = str(current.get("name") or self.entity_id)
