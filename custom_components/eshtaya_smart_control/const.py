674ff8864b3da117c6b81d528e4c69255fecb06f
badereshtaya <badereshtaya95@gmail.com>
2026-09-04 11:37:47 +0300

    Bump integration version to 2.4.4

--- custom_components/eshtaya_smart_control/const.py
+++ custom_components/eshtaya_smart_control/const.py
@@ -5,7 +5,7 @@ from typing import Final
 
 DOMAIN: Final = "eshtaya_smart_control"
 NAME: Final = "Eshtaya Smart Control"
-VERSION: Final = "2.4.3"
+VERSION: Final = "2.4.4"
 MANUFACTURER: Final = "Eshtaya Smart"
 
 DATA_ENTRY: Final = "entry"
